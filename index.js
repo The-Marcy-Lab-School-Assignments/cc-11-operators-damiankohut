65794c4026d1bc29db53eed05c5175f3eb3befb5
damian <damiankohut@gmail.com>
2021-11-02 14:31:25 +0000

    finish everything

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 //1
-function areBothSame(){
-  
+function areBothSame(x,y){
+  if (x == y){
+   return (true) 
+    }else {
+      return (false)
+  }
 }
-
+console.log(areBothSame("2", 2))
 //2
-function areBothEqual(){
-  
+function areBothEqual(x,y){
+    if (x === y){
+   return (true) 
+    }else {
+      return (false)
+  }
 }
-
+console.log(areBothEqual(7,7))
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(w,x,y,z){
+  if (x === y && z === w){
+      return (true)
+  }else {
+      return (false)
+  }
 }
-
+console.log(areAllFourEqual("2", "2", "2", 2))
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(x,y,w,z){
+  if (x == y && z == w){
+      return (true)
+  }else {
+      return (false)
+  }
 }
-
+console.log(areAllFourSame(true, true, 1, "1"))
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(x,y,w,z){
+  if ( x === y && w === z ) {
+      return ("equal")
+  } else if (x == y && w == z){
+      return "same"
+  } else {
+   return null 
+  }
 }
-
+console.log(areAllFourSameOrEqual(true, false, 1, "1"))
 
